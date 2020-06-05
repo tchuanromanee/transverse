23166915db9c10e2b3f9d8b5cb76c8a89bf89c7e
Tya Chuanromanee <tchuanro@nd.edu>
2020-06-05 17:01:49 -0400

    Add function calls for menu interaction

--- app/src/main/java/com/example/transverse/MainActivity.java
+++ app/src/main/java/com/example/transverse/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.transverse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bottom_navigation_menu, menu);
+        return true;
+    }
+
+    //Detect user interaction
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) { //getItemID will say what was selected
+            case R.id.bottomNavigationAddnewMenuId:
+                addNewEntry();
+                return true;
+            case R.id.bottomNavigationEncyclopediaMenuId:
+                showEncyclopedia();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        //respond to menu item selection
+
     }
 }
