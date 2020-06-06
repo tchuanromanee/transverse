7badab9760e8bbfb3af716ebdf1da9f7fba70906
Tya Chuanromanee <tchuanro@nd.edu>
2020-06-06 14:51:27 -0400

    Switch fragments when nav buttons are pressed'

--- app/src/main/java/com/example/transverse/MainActivity.java
+++ app/src/main/java/com/example/transverse/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.transverse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new EncyclopediaFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.bottomNav);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,20 +45,28 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    //Detect user interaction
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) { //getItemID will say what was selected
-            case R.id.bottomNavigationAddnewMenuId:
-                //addNewEntry();
-                return true;
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
             case R.id.bottomNavigationEncyclopediaMenuId:
-                //showEncyclopedia();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+                fragment = new EncyclopediaFragment();
+                break;
+
+            case R.id.bottomNavigationAddnewMenuId:
+                fragment = new AddNewEntryFragment();
+                break;
+
+            case R.id.bottomNavigationStatisticsMenuId:
+                fragment = new StatisticsFragment();
+                break;
+
+            case R.id.bottomNavigationSelfHelpMenuId:
+                fragment = new SelfHelpFragment();
+                break;
         }
-
-        //respond to menu item selection
-
+        return loadFragment(fragment);
     }
 }
