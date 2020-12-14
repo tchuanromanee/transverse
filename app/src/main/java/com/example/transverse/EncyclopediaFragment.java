e2f7645193decc5ff03aa0204cccf77fa0186e91
Tya Chuanromanee <tchuanro@nd.edu>
2020-12-14 17:49:10 -0500

    Dynamically add encyclopedia entriest from assets file in JSON

--- app/src/main/java/com/example/transverse/EncyclopediaFragment.java
+++ app/src/main/java/com/example/transverse/EncyclopediaFragment.java
@@ -12,8 +12,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,7 +41,6 @@ public class EncyclopediaFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private TextView genderBasics;
 
     public EncyclopediaFragment() {
         // Required empty public constructor
@@ -67,7 +76,71 @@ public class EncyclopediaFragment extends Fragment {
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-        genderBasics = (TextView) getView().findViewById(R.id.gender_basic);
+        // Create textviews dynamically depending on encyclopedia.json
+        //Each array name would be a header and a different group
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset("encyclopedia.json"));
+            JSONArray basicsEntries = obj.getJSONArray("basics");
+            JSONArray faqEntries = obj.getJSONArray("faqs");
+
+            LinearLayout.LayoutParams params=new LinearLayout.LayoutParams
+                    ((int) LinearLayout.LayoutParams.WRAP_CONTENT,(int) LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = 50;
+            params.topMargin  = 50;
+
+
+            LinearLayout encyLinearLayout = (LinearLayout) getView().findViewById(R.id.encyclopedia_linearlayout);
+            TextView basicsHeader = new TextView(getContext());
+            basicsHeader.setText("Basics");
+            basicsHeader.setPadding(10, 10, 10, 18);
+            basicsHeader.setTextSize(24);
+            basicsHeader.setLayoutParams(params);
+            encyLinearLayout.addView(basicsHeader);
+
+            for (int i = 0; i < basicsEntries.length(); i++) {
+                JSONObject jo_inside = basicsEntries.getJSONObject(i);
+                String encyEntryname = jo_inside.getString("name");
+                String encyEntryID = jo_inside.getString("id");
+                String encryStringXMLID = jo_inside.getString("stringXMLID");
+                //TODO: Add textview under header and set onclick listener
+                TextView newTextView = new TextView(getContext());
+                newTextView.setText(encyEntryname);
+                newTextView.setPadding(10, 10, 10, 18);
+                newTextView.setTextSize(16);
+                newTextView.setLayoutParams(params);
+                encyLinearLayout.addView(newTextView);
+                }
+
+
+            TextView faqHeader = new TextView(getContext());
+            faqHeader.setText("FAQs");
+            faqHeader.setPadding(10, 10, 10, 18);
+            faqHeader.setTextSize(24);
+            faqHeader.setLayoutParams(params);
+            encyLinearLayout.addView(faqHeader);
+
+
+            for (int i = 0; i < faqEntries.length(); i++) {
+                JSONObject jo_inside = faqEntries.getJSONObject(i);
+                String encyEntryname = jo_inside.getString("name");
+                String encyEntryID = jo_inside.getString("id");
+                String encryStringXMLID = jo_inside.getString("stringXMLID");
+                //TODO: Add textview under header and set onclick listener
+                TextView newTextView = new TextView(getContext());
+                newTextView.setText(encyEntryname);
+                newTextView.setPadding(10, 10, 10, 18);
+                newTextView.setTextSize(16);
+                newTextView.setLayoutParams(params);
+                encyLinearLayout.addView(newTextView);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //genderBasics = (TextView) getView().findViewById(R.id.gender_basic);
 
        /* genderBasics.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +154,22 @@ public class EncyclopediaFragment extends Fragment {
             }
         });*/
 
+    }
+
+    public String loadJSONFromAsset(String fileName) {
+        String json = null;
+        try {
+            InputStream is = getActivity().getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 
     @Override
